d9fb386e861320564bb0a446bc529b537d17249f
achyutvarma <67769612+achyutvarma@users.noreply.github.com>
2025-05-30 11:24:39 +0530

    Create lambda.py

---
+++ list_s3buckets/lambda.py
@@ -0,0 +1,13 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    s3=boto3.resource('s3')
+    bucket_names = []
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+        bucket_names.append(bucket.name)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(bucket_names)
+    }
